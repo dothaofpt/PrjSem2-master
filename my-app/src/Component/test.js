f34034dd112b74e59f133648df60eaf965b1ec17
dothaofpt <143236377+dothaofpt@users.noreply.github.com>
2024-06-04 08:36:10 +0700

    up

--- my-app/src/Component/test.js
+++ my-app/src/Component/test.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const test = () => {
-    return (
-        <div>
-            <h2>abc</h2>
-            <br />
-        </div>
-    );
+  return (
+    <div>
+      <h2>abc</h2>
+      <br />
+      <h3>asd</h3>
+    </div>
+  );
 };
 
 export default test;
